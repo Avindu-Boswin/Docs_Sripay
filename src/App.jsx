9afb079cdb982c3d08408a0749f369ce14bfd5ce
Avindu Payagalage <avindu.k@boswingroup.com>
2025-07-21 09:16:04 +0530

    initial pkg

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Alert, Button } from 'antd';
+
+
+function App() {
+
+  return (
+   <div>
+    <Button>Default Button</Button>
+   </div>
+  )
+}
+
+export default App
