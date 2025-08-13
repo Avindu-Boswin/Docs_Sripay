a16bf1ea85b08333b5f363f734a3e734e778c36d
nilanboswin <nilan.h@boswingroup.com>
2025-08-13 14:11:44 +0530

    Landing Page Developed adding two sections

---
+++ src/components/ThirdSection.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Laptop from '../assets/Macbook-Air-docs.sripay.lk.png';
+import { useNavigate } from 'react-router-dom';
+
+function ThirdSection() {
+
+    const navigate = useNavigate();
+
+    
+    return (
+        <div className="px-4 sm:px-8 lg:px-[23%] w-full py-20">
+  <div className="mx-auto flex flex-col items-center text-center">
+    {/* Badge */}
+    
+
+    {/* Heading */}
+    <h2 className="mt-2 text-[2rem] sm:text-[3rem] md:text-[4rem] lg:text-[4.7rem] font-bold leading-[1.1] tracking-tight">
+      Build with SriPay
+    </h2>
+    <p className=" text-gray-600 text-base sm:text-lg leading-relaxed">
+              SriPay  Documentation is your end‑to‑end guide to integrate
+              secure, reliable payments. Explore REST APIs, authentication flows, webhooks,
+              and best practices to ship faster with confidence.
+            </p>
+
+    {/* Image below */}
+    <img
+      src={Laptop}
+      alt="SriPay Developer Docs Preview"
+      className="mt-8 w-full max-w-[800px] h-auto drop-shadow-2xl rounded-xl "
+      loading="lazy"
+    />
+  </div>
+</div>
+
+    )
+}
+
+export default ThirdSection
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { Outlet } from 'react-router-dom';
-import DeveloperHeading from '../components/DeveloperHeading';
+
 
 const HomeLayout = () => (
   <div>
@@ -10,9 +10,11 @@ const HomeLayout = () => (
     <main>
  
      <Outlet/>
-     
+
+        
+  
     </main>
-   
+
     <Footer />
   </div>
 );
